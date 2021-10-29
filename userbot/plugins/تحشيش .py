d92f3b3b88e1f2eb9703124f32f2c9d782229cb8
ccc1cic <92039671+ccc1cic@users.noreply.github.com>
2021-10-30 01:06:39 +0300

    Add files via upload

---
+++ userbot/plugins/تحشيش .py
@@ -0,0 +1,69 @@
+import random
+
+from Jmthon.razan.resources.strings import *
+from userbot import jmthon
+
+from ..core.managers import edit_or_reply
+from ..helpers import get_user_from_event
+
+plugin_category = "utils"
+
+
+@jmthon.ar_cmd(
+    pattern="نسبة الحب(?:\s|$)([\s\S]*)",
+    command=("نسبة الحب", plugin_category),
+)
+async def permalink(mention):
+    """Generates a link to the user's PM with a custom text."""
+    user, custom = await get_user_from_event(mention)
+    if not user:
+        return
+    muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    rza = random.choice(roz)
+    await edit_or_reply(
+        mention, f"نـسـبتكم انـت و [{muh}](tg://user?id={user.id}) هـي {rza} 😔🖤"
+    )
+
+
+@jmthon.ar_cmd(
+    pattern="نسبة الانوثة(?:\s|$)([\s\S]*)",
+    command=("نسبة الانوثة", plugin_category),
+)
+async def permalink(mention):
+    """Generates a link to the user's PM with a custom text."""
+    user, custom = await get_user_from_event(mention)
+    if not user:
+        return
+    if user.id == 1715051616:
+        return await edit_or_reply(mention, f"**- لكك دي هذا المطور زلمة وعلى راسك**")
+    if user.id == 1694386561:
+        return await edit_or_reply(mention, f"**- لكك دي هذا المطور زلمة وعلى راسك**")
+    if user.id == 1657933680:
+        return await edit_or_reply(mention, f"**- لكك دي هذا المطور زلمة وعلى راسك**")
+    muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    sos = random.choice(rr7)
+    await edit_or_reply(
+        mention, f"⌯︙نسبة الانوثة لـ [{muh}](tg://user?id={user.id}) هـي {sos} 🥵🖤"
+    )
+
+
+@jmthon.ar_cmd(
+    pattern="نسبة الغباء(?:\s|$)([\s\S]*)",
+    command=("نسبة الغباء", plugin_category),
+)
+async def permalink(mention):
+    """Generates a link to the user's PM with a custom text."""
+    user, custom = await get_user_from_event(mention)
+    if not user:
+        return
+    if user.id == 1715051616:
+        return await edit_or_reply(mention, f"**0% ♥🙂**")
+    if user.id == 1694386561:
+        return await edit_or_reply(mention, f"**0% ♥🙂**")
+    if user.id == 1657933680:
+        return await edit_or_reply(mention, f"**0% ♥🙂**")
+    muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    rzona = random.choice(rr7)
+    await edit_or_reply(
+        mention, f"نسبة الغباء لـ [{muh}](tg://user?id={user.id}) هـي {rzona} 😂💔"
+    )
